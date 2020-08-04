0ca613ab6a65b9551e83cede1b9edf597138a482
Catriona <catrionamairi@blueyonder.co.uk>
2020-08-04 17:32:57 +0100

    Tidy up app.rb

--- app.rb
+++ app.rb
@@ -15,17 +15,15 @@ class MakersBnB < Sinatra::Base
   get '/' do
     @listings = Listing.all
     @user = User.find(id: session[:user_id])
-    # flash[:welcome_user] = "Registration successful. Please sign in!"
-    erb :index
+    erb (:index)
   end
 
   get '/listings/new' do
     redirect '/' if User.current_user == nil
-    erb :'listings/new'
+    erb (:'listings/new')
   end
 
   get '/listings/:id/show' do
-    # p params
     @listing = Listing.find(id: params[:id])
     @unicorn = @listing.available_dates
     @user = User.find(id: @listing.user_id)
@@ -34,7 +32,6 @@ class MakersBnB < Sinatra::Base
   end
 
   post '/listings' do
-    # @user = User.find(id: session[:user_id])
     listing = Listing.create(name: params[:name], description: params[:description], price: params[:price])
     Picture.create(url: params[:picture_url], listing_id: listing.id)
     Available_Dates.create(listing_id: listing.id, date_start: params[:date_start], date_end: params[:date_end])
@@ -43,21 +40,19 @@ class MakersBnB < Sinatra::Base
   end
 
   get "/users/:username/user" do
-    # p params
     @user = User.current_user
     @listings = @user.listings
     session[:username] = @user.username
-    erb :'/users/user'
+    erb (:'/users/user')
   end
 
   get "/listings/:id/edit" do
     @listing_id = params[:id]
     @listing = Listing.find(id: @listing_id)
-    erb :'listings/edit'
+    erb (:'listings/edit')
   end
 
   patch '/listings/:id' do
-    # p "Patch #{params}"
     listing = Listing.update(id: params[:id], name: params[:name], description: params[:description], price: params[:price])
     Picture.update(url: params[:url], listing_id: listing.id)
     redirect "/users/#{session[:username]}/user"
@@ -70,10 +65,9 @@ class MakersBnB < Sinatra::Base
     redirect "/users/#{session[:username]}/user"
   end
 
-
   get '/signup' do
     flash[:warning] = "this username/email already exists"
-    erb :signup
+    erb (:signup)
   end
 
   post '/register' do
@@ -84,12 +78,12 @@ class MakersBnB < Sinatra::Base
       redirect '/signup'
     end
     session[:user_id] = user.id
+    flash[:welcome_user] = "Registration successful. Please sign in!"
     redirect '/'
   end
 
   get '/sessions/new' do
-    # p session[:user_id]
-    erb :'/sessions/new'
+    erb (:'/sessions/new')
   end
 
   post '/sessions' do
@@ -97,7 +91,6 @@ class MakersBnB < Sinatra::Base
 
     if user
       session[:user_id] = user.id
-      # flash[:welcome_user] = "Welcome #{user.username}!"
       redirect '/'
     else
       flash[:notice] = 'Please check your username or password!'
@@ -132,30 +125,3 @@ class MakersBnB < Sinatra::Base
 
   run! if app_file == $0
 end
-
-
-
-
-# get '/listings/:id/enquiry' do
-  #   @listing_id = params[:id]
-  #   erb (:'listings/enquiry')
-  # end
-
-  # post '/listings/:id/enquiry' do
-  #   # @listing_id = params[:id]
-  #   # @listing = Listing.find(id: @listing_id) #instance of listing 
-  #   # @listing_owner_id = @listing.user_id #user ID of owns the listing 
-  #   # @listing_owner = User.find(id: @listing_owner_id)
-  #   # @listing_owner_email = @listing_owner.email
-  #   # @enquiry_user = User.current_user.email
-  #   # @enquiry_user_email = @enquiry_user.email 
-  #   #user id making enquire /// user id of listing owner 
-
-  #   #Enquiry.create(subject: params[:subject_line], body: params[:body], to: "#{@listing_owner_email}", from: @enquiry_user_email)
-  #   redirect "/listings/#{@listing_id}/enquiry_confirmation"
-  # end
-
-  # get '/listings/:id/enquiry_confirmation' do
-  #   @listing_id = params[:id]
-  #   erb (:'listings/enquiry_confirmation')
-  # end

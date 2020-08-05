bd8087184ab8ce3c48b8294ac54760e4ae1756bb
Catriona <catrionamairi@blueyonder.co.uk>
2020-08-05 15:12:19 +0100

    Fix rubocop issues in unit tests

--- spec/unit/picture_spec.rb
+++ spec/unit/picture_spec.rb
@@ -21,7 +21,7 @@ describe Picture do
 
       updated_picture = Picture.update(url: 'https://live.staticflickr.com/8651/29886969203_d3d69bac5e_w.jpg', listing_id: "#{listing.id}")
 
-      #expect(updated_picture.id).to eq picture.id
+      expect(updated_picture.id).to eq picture.id
       expect(updated_picture.listing_id).to eq listing.id
       expect(updated_picture.url).to eq 'https://live.staticflickr.com/8651/29886969203_d3d69bac5e_w.jpg'
     end
@@ -31,12 +31,12 @@ describe Picture do
     it 'deletes picture from the database' do
       user_sign_in
       listing = Listing.create(name: 'Minerva', description: 'A really fun place on the surface of the sun', price: '25.00')
-      picture = Picture.create(url: 'https://live.staticflickr.com/4159/33385628794_b912df519b_m.jpg', listing_id: "#{listing.id}")
-      picture_1 = Picture.create(url: 'https://live.staticflickr.com/8651/29886969203_d3d69bac5e_w.jpg', listing_id: "#{listing.id}" )
+      Picture.create(url: 'https://live.staticflickr.com/4159/33385628794_b912df519b_m.jpg', listing_id: "#{listing.id}")
+      Picture.create(url: 'https://live.staticflickr.com/8651/29886969203_d3d69bac5e_w.jpg', listing_id: "#{listing.id}")
       Picture.delete(listing_id: listing.id)
       search = DatabaseConnection.query("SELECT * FROM pictures WHERE listing_id = '#{listing.id}'")
 
-      expect(search.first).to be nil 
+      expect(search.first).to be nil
     end
   end
 end
--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -4,42 +4,30 @@ require 'database_helpers'
 describe User do
   describe '.create' do
     it 'creates a new user' do
-      user = User.create(
-        email: 'email@email.com',
-        password: 'Password123',
-        username: 'username'
-      )
+      user = User.create(username: 'username', email: 'email@email.com', password: 'test_password')
       persisted_data = persisted_data(table: :users, id: user.id)
 
       expect(user).to be_a User
       expect(user.id).to eq persisted_data.first['id']
-      expect(user.email).to eq 'email@email.com'
       expect(user.username).to eq 'username'
+      expect(user.email).to eq 'email@email.com'
     end
 
     it 'hashes the password using BCrypt' do
-      expect(BCrypt::Password).to receive(:create).with('Password123')
+      expect(BCrypt::Password).to receive(:create).with('test_password')
 
-      User.create(
-        email: 'email@email.com',
-        password: 'Password123',
-        username: 'username'
-      )
+      User.create(username: 'username', email: 'email@email.com', password: 'test_password')
     end
   end
 
   describe '.find' do
     it 'finds a user by ID' do
-      user = User.create(
-        email: 'email@email.com',
-        password: 'Password123',
-        username: 'username'
-      )
+      user = User.create(username: 'username', email: 'email@email.com', password: 'test_password')
       result = User.find(id: user.id)
 
       expect(result.id).to eq user.id
-      expect(result.email).to eq user.email
       expect(result.username).to eq user.username
+      expect(result.email).to eq user.email
     end
 
     it 'returns nil if there is no ID given' do
@@ -47,64 +35,57 @@ describe User do
     end
   end
 
-    describe '.authenticate' do
-      it 'returns a user if the credentials match' do
-        user = User.create(username: 'username', email: 'email@email.com', password: 'test_password')
-        authenticated_user = User.authenticate(username: 'username',password: 'test_password')
+  describe '.authenticate' do
+    it 'returns a user if the credentials match' do
+      user = User.create(username: 'username', email: 'email@email.com', password: 'test_password')
+      authenticated_user = User.authenticate(username: 'username', password: 'test_password')
 
-        expect(authenticated_user.id).to eq user.id
-      end
-
-      it 'returns nil given an incorrect username' do
-        user = User.create(username: 'username', email: 'email@email.com', password: 'test_password')
-        authenticated_user = User.authenticate(username: 'wrong_username', password: 'test_password')
-
-        expect(authenticated_user).to be_nil
-      end
-
-      it 'returns nil given an incorrect password' do
-        user = User.create(username: 'username', email: 'email@email.com', password: 'test_password')
-        authenticated_user = User.authenticate(username: 'username', password: 'wrong_password')
-
-        expect(authenticated_user).to be_nil
-      end
-
-      it 'sets current user' do
-        user = User.create(username: 'username', email: 'email@email.com', password: 'test_password')
-        authenticated_user = User.authenticate(username: 'username',password: 'test_password')
-
-        expect(User.current_user).to eq authenticated_user
-      end
+      expect(authenticated_user.id).to eq user.id
     end
 
-    describe '.current_user' do
-      it 'sets current user' do
-        user = User.create(username: 'username', email: 'email@email.com', password: 'test_password')
-        authenticated_user = User.authenticate(username: 'username',password: 'test_password')
+    it 'returns nil given an incorrect username' do
+      User.create(username: 'username', email: 'email@email.com', password: 'test_password')
+      authenticated_user = User.authenticate(username: 'wrong_username', password: 'test_password')
 
-        expect(User.current_user).to eq authenticated_user
-      end
-
-      it 'resets current user after sign out' do
-        user = User.create(username: 'username', email: 'email@email.com', password: 'test_password')
-        authenticated_user = User.authenticate(username: 'username',password: 'test_password')
-        User.sign_out
-
-        expect(User.current_user).to eq nil
-      end
+      expect(authenticated_user).to be_nil
     end
+
+    it 'returns nil given an incorrect password' do
+      User.create(username: 'username', email: 'email@email.com', password: 'test_password')
+      authenticated_user = User.authenticate(username: 'username', password: 'wrong_password')
+
+      expect(authenticated_user).to be_nil
+    end
+  end
+
+  describe '.current_user' do
+    it 'sets current user' do
+      User.create(username: 'username', email: 'email@email.com', password: 'test_password')
+      authenticated_user = User.authenticate(username: 'username', password: 'test_password')
+
+      expect(User.current_user).to eq authenticated_user
+    end
+
+    it 'resets current user after sign out' do
+      User.create(username: 'username', email: 'email@email.com', password: 'test_password')
+      User.authenticate(username: 'username', password: 'test_password')
+      User.sign_out
+
+      expect(User.current_user).to eq nil
+    end
+  end
 
   describe '#listings' do
     it 'gets a array of all listings of a particular user' do
-      user = User.create(username: 'username', email: 'email@email.com', password: 'test_password')
-      authenticated_user = User.authenticate(username: 'username',password: 'test_password')
-      listing = Listing.create(name: 'Boat House', description: 'A boat house on the shores of lake Loch Ness', price: '35.00')
+      User.create(username: 'username', email: 'email@email.com', password: 'test_password')
+      authenticated_user = User.authenticate(username: 'username', password: 'test_password')
+      Listing.create(name: 'Boat House', description: 'A boat house on the shores of lake Loch Ness', price: '35.00')
       listings = authenticated_user.listings
 
-      expect(listings.length).to eq (1)
-      expect(listings.first.name).to eq("Boat House")
-      expect(listings.first.description).to eq("A boat house on the shores of lake Loch Ness")
-      expect(listings.first.price).to eq("35.00")
+      expect(listings.length).to eq 1
+      expect(listings.first.name).to eq "Boat House"
+      expect(listings.first.description).to eq "A boat house on the shores of lake Loch Ness"
+      expect(listings.first.price).to eq "35.00"
     end
   end
 end
